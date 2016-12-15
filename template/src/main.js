25a5f03a74da882c8d122cabc03fa16b4379001e
saber-qianqian <yizhihuji@gmail.com>
2016-12-15 16:55:16 +0800

    修改配置

--- template/src/main.js
+++ template/src/main.js
@@ -7,12 +7,12 @@ import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  {{#if_eq build "runtime"}}
-  render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  template: '<App/>',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
+	el: '#app',
+	{{#if_eq build "runtime"}}
+	render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+	{{/if_eq}}
+	{{#if_eq build "standalone"}}
+	template: '<App/>',
+	components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+	{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
